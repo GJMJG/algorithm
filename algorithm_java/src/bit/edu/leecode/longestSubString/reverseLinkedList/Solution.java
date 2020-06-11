7581ee01ff4ed0fadcba4c154eb35a19de01e1aa
GuoJM <guojmbucter@163.com>
2020-06-11 09:32:08 +0800

    增加题目：反转链表

---
+++ algorithm_java/src/bit/edu/leecode/longestSubString/reverseLinkedList/Solution.java
@@ -0,0 +1,29 @@
+package bit.edu.leecode.longestSubString.reverseLinkedList;
+
+/**
+ * Definition for singly-linked list.
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode currentNode = head;
+        ListNode preNode = null;
+
+        while (currentNode != null) {
+            ListNode nextNode = currentNode.next;
+            currentNode.next = preNode;
+            preNode = currentNode;
+            currentNode = nextNode;
+        }
+        return preNode;
+    }
+}
